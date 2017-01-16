e56e5a98332277bba01a5101752b82169687f059
Didier Wenzek <didier.wenzek@acidalie.com>
2017-01-16 15:27:23 +0100

    Add exception wrapper for traildb errors.

--- trailDB.ml
+++ trailDB.ml
@@ -14,6 +14,60 @@ type literal = Pos of tdb_item | Neg of tdb_item
 type disjunction = Or of literal list
 type conjunction = And of disjunction list
 
+type error = 
+    (* generic *)
+    | TDB_ERR_NOMEM
+    | TDB_ERR_PATH_TOO_LONG
+    | TDB_ERR_UNKNOWN_FIELD
+    | TDB_ERR_UNKNOWN_UUID
+    | TDB_ERR_INVALID_TRAIL_ID
+    | TDB_ERR_HANDLE_IS_NULL
+    | TDB_ERR_HANDLE_ALREADY_OPENED
+    | TDB_ERR_UNKNOWN_OPTION
+    | TDB_ERR_INVALID_OPTION_VALUE
+    | TDB_ERR_INVALID_UUID
+
+    (* io *)
+    | TDB_ERR_IO_OPEN
+    | TDB_ERR_IO_CLOSE
+    | TDB_ERR_IO_WRITE
+    | TDB_ERR_IO_READ
+    | TDB_ERR_IO_TRUNCATE
+    | TDB_ERR_IO_PACKAGE
+
+    (* tdb_open *)
+    | TDB_ERR_INVALID_INFO_FILE
+    | TDB_ERR_INVALID_VERSION_FILE
+    | TDB_ERR_INCOMPATIBLE_VERSION
+    | TDB_ERR_INVALID_FIELDS_FILE
+    | TDB_ERR_INVALID_UUIDS_FILE
+    | TDB_ERR_INVALID_CODEBOOK_FILE
+    | TDB_ERR_INVALID_TRAILS_FILE
+    | TDB_ERR_INVALID_LEXICON_FILE
+    | TDB_ERR_INVALID_PACKAGE
+
+    (* tdb_cons *)
+    | TDB_ERR_TOO_MANY_FIELDS
+    | TDB_ERR_DUPLICATE_FIELDS
+    | TDB_ERR_INVALID_FIELDNAME
+    | TDB_ERR_TOO_MANY_TRAILS
+    | TDB_ERR_VALUE_TOO_LONG
+    | TDB_ERR_APPEND_FIELDS_MISMATCH
+    | TDB_ERR_LEXICON_TOO_LARGE
+    | TDB_ERR_TIMESTAMP_TOO_LARGE
+    | TDB_ERR_TRAIL_TOO_LONG
+
+    (* querying *)
+    | TDB_ERR_ONLY_DIFF_FILTER
+    | TDB_ERR_NO_SUCH_ITEM
+    | TDB_ERR_INVALID_RANGE
+    | TDB_ERR_INCORRECT_TERM_TYPE
+
+exception Error of error
+
+let _ =
+  Callback.register_exception "traildb.error" (Error(TDB_ERR_NOMEM))
+
 external tdb_cons_open: path -> field_name list -> tdb_cons = "ocaml_tdb_cons_open"
 external tdb_cons_add: tdb_cons -> uuid -> timestamp -> field_value list -> unit = "ocaml_tdb_cons_add"
 external tdb_cons_finalize: tdb_cons -> unit = "ocaml_tdb_cons_finalize"
@@ -34,5 +88,5 @@ external tdb_get_trail: tdb_cursor -> trail_id -> unit = "ocaml_tdb_get_trail"
 external tdb_get_trail_length: tdb_cursor -> int64 = "ocaml_tdb_get_trail_length"
 external tdb_cursor_next: tdb_cursor -> tdb_event option = "ocaml_tdb_cursor_next"
 external tdb_cursor_peek: tdb_cursor -> tdb_event option = "ocaml_tdb_cursor_peek"
-external tdb_event_filter_new: conjunction -> tdb_event_filter = "ocaml_event_filter_new"
-
+external tdb_event_filter_new: conjunction -> tdb_event_filter = "ocaml_tdb_event_filter_new"
+external tdb_error_str: error -> string = "ocaml_tdb_error_str"
--- trailDB.mli
+++ trailDB.mli
@@ -119,3 +119,64 @@ type conjunction = And of disjunction list
 
 val tdb_event_filter_new: conjunction -> tdb_event_filter
 
+(* ----------------------------------------------------- *)
+(* Working with errors.                                  *)
+(*                                                       *)
+(* All functions may raise exceptions of the Error type. *)
+(* ----------------------------------------------------- *)
+
+(* Error codes *)
+type error = 
+  (* generic *)
+  | TDB_ERR_NOMEM
+  | TDB_ERR_PATH_TOO_LONG
+  | TDB_ERR_UNKNOWN_FIELD
+  | TDB_ERR_UNKNOWN_UUID
+  | TDB_ERR_INVALID_TRAIL_ID
+  | TDB_ERR_HANDLE_IS_NULL
+  | TDB_ERR_HANDLE_ALREADY_OPENED
+  | TDB_ERR_UNKNOWN_OPTION
+  | TDB_ERR_INVALID_OPTION_VALUE
+  | TDB_ERR_INVALID_UUID
+  
+  (* io *)
+  | TDB_ERR_IO_OPEN
+  | TDB_ERR_IO_CLOSE
+  | TDB_ERR_IO_WRITE
+  | TDB_ERR_IO_READ
+  | TDB_ERR_IO_TRUNCATE
+  | TDB_ERR_IO_PACKAGE
+  
+  (* tdb_open *)
+  | TDB_ERR_INVALID_INFO_FILE
+  | TDB_ERR_INVALID_VERSION_FILE
+  | TDB_ERR_INCOMPATIBLE_VERSION
+  | TDB_ERR_INVALID_FIELDS_FILE
+  | TDB_ERR_INVALID_UUIDS_FILE
+  | TDB_ERR_INVALID_CODEBOOK_FILE
+  | TDB_ERR_INVALID_TRAILS_FILE
+  | TDB_ERR_INVALID_LEXICON_FILE
+  | TDB_ERR_INVALID_PACKAGE
+  
+  (* tdb_cons *)
+  | TDB_ERR_TOO_MANY_FIELDS
+  | TDB_ERR_DUPLICATE_FIELDS
+  | TDB_ERR_INVALID_FIELDNAME
+  | TDB_ERR_TOO_MANY_TRAILS
+  | TDB_ERR_VALUE_TOO_LONG
+  | TDB_ERR_APPEND_FIELDS_MISMATCH
+  | TDB_ERR_LEXICON_TOO_LARGE
+  | TDB_ERR_TIMESTAMP_TOO_LARGE
+  | TDB_ERR_TRAIL_TOO_LONG
+  
+  (* querying *)
+  | TDB_ERR_ONLY_DIFF_FILTER
+  | TDB_ERR_NO_SUCH_ITEM
+  | TDB_ERR_INVALID_RANGE
+  | TDB_ERR_INCORRECT_TERM_TYPE
+
+(* TrailDB exceptions *)
+exception Error of error
+
+(* Translate an error code to a string. *)
+val tdb_error_str: error -> string
